de46f6477873e96d5e72a2f95cc6dbef4153fbf4
Anand Mohan Pathak <anandpathak8317@gmail.com>
2024-01-20 19:57:07 +0530

    linters fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_comments_counter
-  
-  private
 
   def self.update_comments_counter(post_id)
     Post.increment_counter(:comments_counter, post_id)
